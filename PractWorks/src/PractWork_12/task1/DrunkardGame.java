4cfd27fea03d778bdc23f87ea60a788345bff466
Andrey <ae.nalimov@mail.ru>
2023-10-29 19:41:59 +0300

    Pract12 task1 init commit

---
+++ PractWorks/src/PractWork_12/task1/DrunkardGame.java
@@ -0,0 +1,48 @@
+package PractWork_12.task1;
+
+import java.util.Scanner;
+import java.util.ArrayDeque;
+
+public class DrunkardGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String player1Cards = scanner.nextLine();
+        String player2Cards = scanner.nextLine();
+
+        ArrayDeque<Integer> player1Deck = new ArrayDeque<>();
+        ArrayDeque<Integer> player2Deck = new ArrayDeque<>();
+
+        for (int i = 4; i >= 0; i--) {
+            player1Deck.push(Integer.parseInt(player1Cards.substring(i, i + 1)));
+            player2Deck.push(Integer.parseInt(player2Cards.substring(i, i + 1)));
+        }
+
+        int moves = 0;
+
+        while (moves < 106) {
+            int player1Card = player1Deck.pop();
+            int player2Card = player2Deck.pop();
+
+            if (player1Card > player2Card) {
+                player1Deck.add(player1Card);
+                player1Deck.add(player2Card);
+            }
+            else {
+                player2Deck.add(player1Card);
+                player2Deck.add(player2Card);
+            }
+
+            moves++;
+
+            if (player1Deck.isEmpty()) {
+                System.out.println("second " + moves);
+                return;
+            } else if (player2Deck.isEmpty()) {
+                System.out.println("first " + moves);
+                return;
+            }
+        }
+
+        System.out.println("botva");
+    }
+}
