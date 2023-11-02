8763591628e8f8945f33df06648494ac3f66bc1c
Andrey <ae.nalimov@mail.ru>
2023-11-03 00:43:14 +0300

    Pract11 task2 changed array

--- PractWorks/src/PractWork_11/task2/Main.java
+++ PractWorks/src/PractWork_11/task2/Main.java
@@ -2,18 +2,12 @@ package PractWork_11.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Alexander", 200);
-        Student s2 = new Student("Andrey", 325);
-        Student s3 = new Student("Irina", 150);
-        Student s4 = new Student("Gerasim", 300);
-        Student s5 = new Student("Egor", 150);
-
         Student[] students = new Student[5];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-        students[3] = s4;
-        students[4] = s5;
+        students[0] = new Student("Alexander", 200);
+        students[1] = new Student("Andrey", 325);
+        students[2] = new Student("Irina", 150);
+        students[3] = new Student("Gerasim", 300);
+        students[4] = new Student("Egor", 150);
 
         SortingStudentsByGPA sortObj = new SortingStudentsByGPA();
 
