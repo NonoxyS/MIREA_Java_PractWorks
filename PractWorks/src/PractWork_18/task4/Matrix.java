21e4b5b8d0d85d3536adab25c0add02b0a99f2f8
Andrey <ae.nalimov@mail.ru>
2023-12-07 10:32:33 +0300

    Pract18 task4 updated

--- PractWorks/src/PractWork_18/task4/Matrix.java
+++ PractWorks/src/PractWork_18/task4/Matrix.java
@@ -27,6 +27,31 @@ public class Matrix <T> {
         }
     }
 
+    public T[][] addition(T[][] matrice) {
+        try {
+            if ((matrice.length != this.matrice.length) || (matrice[0].length != this.matrice[0].length)) {
+                throw new IllegalArgumentException("Sizes of matrices are not equals");
+            }
+            for (int i = 0; i < matrice.length; i++) {
+                for (int j = 0; j < matrice[i].length; j++) {
+                    this.matrice[i][j] = (T) add((Number)this.matrice[i][j], (Number)matrice[i][j]);
+                }
+            }
+            return matrice;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private Number add(Number a, Number b) {
+        if (a instanceof Double || b instanceof Double) {
+            return a.doubleValue() + b.doubleValue();
+        } else {
+            return a.intValue() + b.intValue();
+        }
+    }
+
     public static void main(String[] args) {
         Integer[][] intMatrice = {
                 {2, 5, 4},
